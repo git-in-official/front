3d08fd37490b210274b2df7ea725dd02ea8ebe3c
lyuhyewon <lyuhw11023@gmail.com>
2024-08-14 04:09:10 +0900

    feat: Api 요청 구현(미완성)(#5)

--- lib/ui/view_model/emotion_view_model.dart
+++ lib/ui/view_model/emotion_view_model.dart
@@ -1,6 +1,8 @@
 import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
 import 'package:to_morrow_front/data/model/emotion_model.dart';
 import 'package:to_morrow_front/repository/emotion_repository.dart';
+import 'dart:convert';
 
 class EmotionViewModel extends GetxController {
   var selectedEmotion = Rxn<EmotionModel>();
@@ -20,8 +22,30 @@ class EmotionViewModel extends GetxController {
 
   void selectEmotion(EmotionModel emotion) async{
     selectedEmotion.value = emotion;
+
+    await sendEmotionToServer(emotion);
+
     // 감정 선택 후 다음 페이지로 이동
     Get.toNamed('/nextPage');
+  }
+
+  Future<void> sendEmotionToServer(EmotionModel emotion) async {
+    final url = Uri.parse('');
+    final response = await http.post(
+      url,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: json.encode({
+        'title': emotion.title,
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      print('서버 연결 성공');
+    } else {
+      print('서버 연결 실패');
+    }
   }
 
 }
