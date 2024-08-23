b4bf0b1b112f8d6a7a888126326f4e03011c0546
hennie <hennie9401@gmail.com>
2024-08-23 10:05:28 +0900

    page_route추가 및 페이지 이동 구현, MaintabController 수정

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:to_morrow_front/repository/global_controller.dart';
 import 'package:to_morrow_front/repository/login_controller.dart';
+import 'package:to_morrow_front/repository/maintab_controller.dart';
 import 'package:to_morrow_front/ui/screens/splash_page/splash_page.dart';
 
 
 void main() {
   runApp(const tomorrow());
+
   Get.put(GlobalController());
   Get.put(LoginController());
+  Get.put(MainTabController());
 }
 
 class  tomorrow extends StatelessWidget {
@@ -21,15 +25,9 @@ class  tomorrow extends StatelessWidget {
     return GetMaterialApp(
       title: "tomorrow",
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-          fontFamily: 'KoPubBatangPro',
-        iconButtonTheme: IconButtonThemeData(
-          style : ButtonStyle(
-            overlayColor: MaterialStateColor.resolveWith((states) => Colors.transparent),
-          )
-        )
-      ),
       home: SplashPage(),
+
     );
+
   }
 }
