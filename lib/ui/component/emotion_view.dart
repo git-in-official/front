024bee766ce17628c93760d7dfdf14a29cca7018
lyuhyewon <lyuhw11023@gmail.com>
2024-08-26 23:39:25 +0900

    파일 이동에 따른 import 경로 수정

--- lib/ui/component/emotion_view.dart
+++ lib/ui/component/emotion_view.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:to_morrow_front/ui/component/write_edit_view.dart';
+import 'package:to_morrow_front/ui/screens/write_edit_page//write_edit_view.dart';
 import 'package:to_morrow_front/ui/view_model/emotion_view_model.dart';
 
 
