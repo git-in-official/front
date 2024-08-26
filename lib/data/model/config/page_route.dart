f90fd1d861d3c82772d605254e9b681895757794
hennie-yun <hennie9401@gmail.com>
2024-08-26 23:42:24 +0900

    page route 수정

--- lib/data/model/config/page_route.dart
+++ lib/data/model/config/page_route.dart
@@ -1,12 +1,15 @@
 //페이지 정의
 import 'package:flutter/cupertino.dart';
 
+import '../../../ui/component/emotion_view.dart';
 import '../../../ui/screens/main_page/main_home_page.dart';
 import '../../../ui/screens/main_page/sound_write.dart';
 import '../../../ui/screens/main_page/title_writing_material.dart';
 import '../../../ui/screens/main_page/video_write.dart';
 import '../../../ui/screens/main_page/word_writing_material.dart';
+import '../../../ui/screens/modal_page/EmotionAnalysisModal.dart';
 import '../../../ui/screens/my_page/my_profile.dart';
+import '../../../ui/screens/write_edit_page/write_edit_view.dart';
 
 //이동할 페이지를 정의
 Map<String, Widget Function(String)> pages = {
@@ -16,4 +19,7 @@ Map<String, Widget Function(String)> pages = {
   'VideoWrite': (_) => VideoWrite(),
   'WordWritingMaterial': (_) => WordWritingMaterial(),
   'SoundWrite': (_) => SoundWrite(),
+  'Emotion' : (_) => EmotionView(),
+  'WriteEdit':(_) =>  WriteEditView(),
+  'EmotionAnalysisModal' :(_) =>  EmotionAnalysisModal()
 };
