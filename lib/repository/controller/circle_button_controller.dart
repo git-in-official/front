d41fce2a5c8ab6f22c99ddafc06b0d6216fb4360
hennie-yun <hennie9401@gmail.com>
2024-08-31 20:35:09 +0900

    시몇번쓸수 있는지 확인하는 api 주소 변경

--- lib/repository/controller/circle_button_controller.dart
+++ lib/repository/controller/circle_button_controller.dart
@@ -1,12 +1,13 @@
+import 'dart:convert';
+
 import 'package:get/get.dart';
 import 'package:get/get_state_manager/src/simple/get_controllers.dart';
 import 'package:http/http.dart' as http;
 
 import 'auth_service.dart';
 
-
 class CircleButtonController extends GetxController {
-  var _baseUrl = 'https://api.leemhoon.com/poems/can-write';
+  var _baseUrl = 'https://api.leemhoon.com/poems/remain';
 
   var canWritePoem = false.obs;
 
@@ -22,7 +23,8 @@ class CircleButtonController extends GetxController {
 
       final authService = AuthService();
       // final token = await authService.loadServiceTokens();
-      final token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjkxOTY2ZWIyLWQ3OTMtNDVmNi04YjE4LWZkM2ZjZjZkYTZhNyIsImlhdCI6MTcyNDU4OTg1OCwiZXhwIjoxNzI3MTgxODU4fQ.JqZMFiY6xUa7nK7lCRFuUdSwXGhQ8gUzUq6JuCsU22I";
+      final token =
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjkxOTY2ZWIyLWQ3OTMtNDVmNi04YjE4LWZkM2ZjZjZkYTZhNyIsImlhdCI6MTcyNDU4OTg1OCwiZXhwIjoxNzI3MTgxODU4fQ.JqZMFiY6xUa7nK7lCRFuUdSwXGhQ8gUzUq6JuCsU22I";
 
       final headers = {
         'Authorization': 'Bearer $token',
@@ -31,12 +33,21 @@ class CircleButtonController extends GetxController {
 
       final response = await http.get(uri, headers: headers);
 
-      print (response.statusCode);
-
-
       if (response.statusCode >= 200 && response.statusCode < 300) {
-        print('시를 쓸 수 있다');
-        canWritePoem.value = true;
+
+        final responseBody = jsonDecode(response.body);
+
+        print (responseBody["count"] );
+
+        if (responseBody["count"] != 0) {
+          print('시를 쓸 수 있다');
+
+          canWritePoem.value = true;
+        } else {
+          print('시 2번 다씀');
+
+          canWritePoem.value = false;
+        }
       } else if (response.statusCode == 400) {
         print('시 2번 다씀');
         canWritePoem.value = false;
