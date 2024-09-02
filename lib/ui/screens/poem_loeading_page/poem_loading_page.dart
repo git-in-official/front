071c4eaf7a6d2fa29b12595e0e08bca8231af144
hennie <hennie9401@gmail.com>
2024-09-02 10:47:25 +0900

    '교정중' 애니메이션 화, 버튼 위치 이상했던 거 수정 (#52)

--- lib/ui/screens/poem_loeading_page/poem_loading_page.dart
+++ lib/ui/screens/poem_loeading_page/poem_loading_page.dart
@@ -1,3 +1,4 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -14,14 +15,18 @@ class PoemLoadingPage extends StatelessWidget {
   final WriteEditViewModel writeEditViewModel = Get.find();
   final MainTabController tabController = Get.find();
 
+  PoemLoadingPage() {
+    _startSendingPoemData();
+  }
+
   void _onConfirmPressed() {
     Get.offAll(() => Maintab());
     tabController.pageName.value = "Home";
   }
 
+
   @override
   Widget build(BuildContext context) {
-    _startSendingPoemData();
     return Scaffold(
       backgroundColor: const Color(0xFFE6E2DB),
       body: Center(
@@ -44,7 +49,7 @@ class PoemLoadingPage extends StatelessWidget {
                   const SizedBox(height: 24),
                   Text(
                     '${writeEditViewModel.userName.value}님의 작품이 \n'
-                    '교정과 교열에 들어갔습니다.\n',
+                        '교정과 교열에 들어갔습니다.\n',
                     textAlign: TextAlign.center,
                     style: const TextStyle(
                       fontSize: 18,
@@ -56,8 +61,8 @@ class PoemLoadingPage extends StatelessWidget {
                   const SizedBox(height: 24),
                   const Text(
                     '인쇄가 완료되면\n'
-                    '내일의 누군가를 위해\n'
-                    '작품이 출판됩니다.\n',
+                        '내일의 누군가를 위해\n'
+                        '작품이 출판됩니다.\n',
                     textAlign: TextAlign.center,
                     style: TextStyle(
                       fontSize: 18,
@@ -71,7 +76,7 @@ class PoemLoadingPage extends StatelessWidget {
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       const Text(
-                        '집필 > 탈고 > ',
+                        '집필 > 탈고 >',
                         style: TextStyle(
                           fontSize: 13,
                           fontFamily: 'KoPubBatangPro',
@@ -81,17 +86,17 @@ class PoemLoadingPage extends StatelessWidget {
                       ),
                       SizedBox(
                         width: 38,
-                        child: Text(
-                          '교정중',
-                          key: ValueKey<int>(_finishWritingPoem.stage.value),
-                          textAlign: TextAlign.start,
-                          style: const TextStyle(
-                            fontSize: 13,
-                            fontFamily: 'KoPubBatangPro',
-                            color: Color(0xFF373430),
-                          ),
-                        ),
-                      ),
+                      child :
+                      AnimatedTextKit(animatedTexts: [
+                        TyperAnimatedText(' 교정중',
+                            speed: const Duration(milliseconds: 200),
+                            textStyle: const TextStyle(
+                              fontSize: 13,
+                              color: Color(0xFF373430),
+                              fontFamily: 'KoPubBatangPro',
+                              fontWeight: FontWeight.bold,
+                            )),
+                      ], repeatForever: true)),
                       const Text(
                         ' > 교열 > 인쇄 > 출판',
                         style: TextStyle(
@@ -103,51 +108,61 @@ class PoemLoadingPage extends StatelessWidget {
                     ],
                   ),
                   const SizedBox(height: 54),
-                  SizedBox(
-                    width: double.infinity,
-                    child: ElevatedButton(
-                      onPressed: _finishWritingPoem.isSendingComplete.value
-                          ? _onConfirmPressed
-                          : null,
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color(0xFF373430),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                        padding: const EdgeInsets.symmetric(vertical: 16),
-                      ),
-                      child: const Text(
-                        '확인',
-                        style: TextStyle(
-                          fontSize: 16.0,
-                          color: Color(0xFFE6E2DB),
-                          fontWeight: FontWeight.w700,
-                          fontFamily: 'KoPubBatangPro',
-                        ),
-                      ),
-                    ),
-                  ),
                 ],
               ),
-              // 멘트
-              Positioned(
-                bottom: 170,
-                left: 10,
-                child: Container(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 12, vertical: 3),
-                  decoration: BoxDecoration(
-                    color: Color(0xFFE6E2DB),
-                    borderRadius: BorderRadius.circular(12),
-                    border: Border.all(
-                      color: const Color(0xFF373430),
-                      width: 1.0,
+              Obx(() => Stack(
+                clipBehavior: Clip.none,
+                alignment: Alignment.center,
+                children: [
+                  Positioned(
+                    bottom: 40,
+                    right: 0,
+                    left: 0,
+                    child: SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: _finishWritingPoem.isSendingComplete.value
+                            ? _onConfirmPressed
+                            : null,
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color(0xFF373430),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          padding: const EdgeInsets.symmetric(vertical: 16),
+                          minimumSize: const Size.fromHeight(50), // 버튼 높이 50으로 고정
+                        ),
+                        child: const Text(
+                          '확인',
+                          style: TextStyle(
+                            fontSize: 16.0,
+                            color: Color(0xFFE6E2DB),
+                            fontWeight: FontWeight.w700,
+                            fontFamily: 'KoPubBatangPro',
+                          ),
+                        ),
+                      ),
                     ),
                   ),
-                  height: 20.0,
-                  width: 145.0,
-                  alignment: Alignment.center,
-                  child: Obx(() => Text(
+                  // 탈고 횟수 표시 부분을 버튼 위에 겹치도록 위치 조정
+                  Positioned(
+                    bottom: 80,
+                    left: 10,
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 12, vertical: 3),
+                      decoration: BoxDecoration(
+                        color: Color(0xFFE6E2DB),
+                        borderRadius: BorderRadius.circular(12),
+                        border: Border.all(
+                          color: const Color(0xFF373430),
+                          width: 1.0,
+                        ),
+                      ),
+                      height: 20.0,
+                      width: 145.0,
+                      alignment: Alignment.center,
+                      child: Obx(() => Text(
                         '오늘 가능한 탈고 횟수: ${_finishWritingPoem.remainingEdits.value}회',
                         style: const TextStyle(
                           fontSize: 10,
@@ -156,26 +171,15 @@ class PoemLoadingPage extends StatelessWidget {
                         ),
                         textAlign: TextAlign.center,
                       )),
-                ),
-              ),
+                    ),
+                  ),
+                ],
+              )),
             ],
           ),
         ),
       ),
     );
-  }
-
-  String _getStageText(int stage) {
-    switch (stage) {
-      case 0:
-        return '교';
-      case 1:
-        return '교정';
-      case 2:
-        return '교정중';
-      default:
-        return '';
-    }
   }
 
   void _startSendingPoemData() {
